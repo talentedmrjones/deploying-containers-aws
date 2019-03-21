1ac9b327ffc4b2e5eff6e5f25933a233ec4c0b7f
Richard Jones <the@talentedmrjones.com>
2019-03-21 12:22:40 -0600

    added commands for messages service

--- cloudformation/ecs-commands.sh
+++ cloudformation/ecs-commands.sh
@@ -44,7 +44,7 @@ ParameterKey=ElbName,ParameterValue=microservices-dev-ohio \
 # if creating ECS service for the first time in the account run the following:
 # aws --region $REGION iam create-service-linked-role --aws-service-name ecs.amazonaws.com
 
-# create the ECS service and task
+# create the ECS service and task for USERS service
 aws --region $REGION cloudformation create-stack \
 --stack-name microservices-service-users \
 --template-body file://./ecs-service-task-users.yml \
@@ -54,12 +54,37 @@ ParameterKey=AlbStack,ParameterValue=microservices-alb-dev-ohio \
 --capabilities "CAPABILITY_NAMED_IAM" \
 && aws --region $REGION cloudformation wait stack-create-complete --stack-name microservices-service-users
 
-# create the CICD pipeline
+# create the CICD pipeline for USERS
 aws --region $REGION cloudformation create-stack \
 --stack-name microservices-pipeline-users \
 --template-body file://./pipeline.yml \
 --parameters \
+ParameterKey=ArtifactBucket,ParameterValue=cerulean-operations-us-east-2 \
 ParameterKey=ClusterName,ParameterValue=microservices-ecs \
 ParameterKey=Service,ParameterValue=users \
 --capabilities "CAPABILITY_NAMED_IAM" \
 && aws --region $REGION cloudformation wait stack-create-complete --stack-name microservices-pipeline-users
+
+# create the ECS service and task for MESSAGES service
+# notice that the messages service requires the additional NetworkStack parameter
+# this is because the FARGATE launch type will create ENIs (elastic network interface) inside OUR VPC
+aws --region $REGION cloudformation create-stack \
+--stack-name microservices-service-messages \
+--template-body file://./ecs-service-task-messages.yml \
+--parameters \
+ParameterKey=NetworkStack,ParameterValue=microservices-network \
+ParameterKey=ClusterName,ParameterValue=microservices-ecs \
+ParameterKey=AlbStack,ParameterValue=microservices-alb-dev-ohio \
+--capabilities "CAPABILITY_NAMED_IAM" \
+&& aws --region $REGION cloudformation wait stack-create-complete --stack-name microservices-service-messages
+
+# create the CICD pipeline for MESSAGES
+aws --region $REGION cloudformation create-stack \
+--stack-name microservices-pipeline-messages \
+--template-body file://./pipeline.yml \
+--parameters \
+ParameterKey=ArtifactBucket,ParameterValue=cerulean-operations-us-east-2 \
+ParameterKey=ClusterName,ParameterValue=microservices-ecs \
+ParameterKey=Service,ParameterValue=messages \
+--capabilities "CAPABILITY_NAMED_IAM" \
+&& aws --region $REGION cloudformation wait stack-create-complete --stack-name microservices-pipeline-messages
