785256017e9daaf3f95d06a18e577ce574c89f5c
Richard Jones <the@talentedmrjones.com>
2019-12-05 14:47:24 +0000

    minor fixes

--- kubernetes/eksctl.sh
+++ kubernetes/eksctl.sh
@@ -7,8 +7,10 @@ eksctl create cluster \
 --nodes-min 3 \
 --nodes-max 3 \
 --node-ami auto \
---vpc-public-subnets=subnet-07e086e0f83a8c03b,subnet-0ec3063c069f0864e,subnet-0a8cfb228ff6bb753 \
---vpc-private-subnets=subnet-0632a9a8a2706f958,subnet-066d8c85e986cd3a2,subnet-00bad100432a5d2ab
+--vpc-public-subnets=subnet-059e2687d914d6c0c,subnet-064c99608a6f339c6,subnet-01903b5cb71c08418 \
+--vpc-private-subnets=subnet-02a4bfac3b04a977d,subnet-0161e512b30974a02,subnet-03e1b184cf43afecd
+
+
 
 # REPLACE the above subnets with YOUR subnets
 
@@ -22,8 +24,12 @@ kubectl rollout status deployment/nginxhello
 
 kubectl get pods --show-labels
 
-kubectl get services
+kubectl describe deployments
 
+kubectl get services
+# there is not service at this point
 kubectl expose deployment nginxhello --type=LoadBalancer --name=helloworld
 
+kubectl get services
+# now we have a service
 kubectl get service helloworld
